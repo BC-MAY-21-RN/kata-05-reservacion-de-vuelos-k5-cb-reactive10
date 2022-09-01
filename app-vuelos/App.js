184c404fe0ab7a86d2c36b4cc03d88fe50e55dd3
Jose Herrera <jherrera892016@gmail.com>
2022-09-01 15:10:01 -0500

    add(core): initialProject
    - Add structure initialProject.
    - Add lottie Animations.
    - Add Colors, Icons.
    
    co-authored-by: XimenaV26 <xvelasco1@ucol.mx>
    co-authored-by: Lucas-aquiles <lucasechegaray2011@gmail.com>

--- app-vuelos/App.js
+++ app-vuelos/App.js
@@ -1,20 +1,77 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, SafeAreaView, View, Platform, Image } from 'react-native';
+import { Colors } from '../app-vuelos/src/theme/Colors'
+import LottieView from 'lottie-react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={{color:'#5C6EF8', fontWeight:'bold', fontSize:20, marginTop: 60}}>Sign Up</Text>
+      
+      <Text >Firts Name:</Text>
+      <TextInput
+      placeholder='Ingrese tu nombre'
+      keyboardType='default'
+      style={styles.input}
+      />
+            <Text >Email:</Text>
+      <TextInput
+      placeholder='Ingrese tu nombre'
+      keyboardType='email-address'
+      style={styles.input}
+      ke
+      />
+            <Text >Password:</Text>
+      <TextInput
+      placeholder='Ingrese tu nombre'
+      keyboardType='default'
+      style={styles.input}
+      secureTextEntry={true}
+      />
+      <View>
+        <Image
+        source={require('./src/assets/g1.png')}
+        style={{width:50, height:50}}
+        />
+      <LottieView
+      loop
+      autoPlay
+      style={{
+      width: 40,
+      height: 40,
+      position:'absolute',
+      marginTop: 5,
+      marginLeft: 150,
+      }}
+      source={require('./assets/login.json')}
+    />
+      </View>
+    
+      
+      
+      
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignSelf:'center',
+    width:'90%',
+    backgroundColor: Colors.primary,
   },
+  input:{
+    paddingLeft:8,
+    alignSelf:'center',
+    width:'100%',
+    height: 50,
+    borderWidth: 1,
+    borderColor: Colors.black,
+    borderRadius:8,
+  },
+
 });
---
+++ app-vuelos/src/theme/Colors.js
@@ -0,0 +1,5 @@
+export const Colors = {
+  primary: '#d1d5db',
+  secundary: '#2ab4c5',
+  black: '#18181b',
+}

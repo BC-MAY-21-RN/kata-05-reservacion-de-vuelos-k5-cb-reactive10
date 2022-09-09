9c8fddb2da22d7abee5e3486e759bc88dab7282b
XimenaV26 <54459424+XimenaV26@users.noreply.github.com>
2022-09-09 18:57:52 -0500

    fix(core): fix style
    -Fix Login styles
    -Create Login styles
    
    Co-Authored-By: Jose Herrera <66761042+featlast@users.noreply.github.com>
    Co-Authored-By: Lucas Manuel <lucasechegaray2011@gmail.com>

--- app-vuelos/src/views/LoginScreen.js
+++ app-vuelos/src/views/LoginScreen.js
@@ -1,4 +1,4 @@
-import Reactm,{useState} from "react";
+import Reactm, { useState } from "react";
 import {
   Image,
   Text,
@@ -8,7 +8,7 @@ import {
   TouchableOpacity,
   TextInput,
   SafeAreaView,
-  Keyboard 
+  Keyboard,
 } from "react-native";
 import { BlurView } from "expo-blur";
 import LottieView from "lottie-react-native";
@@ -16,20 +16,19 @@ import { StatusBar } from "expo-status-bar";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import MyInput from "../components/MyInput";
 import MyButton from "../components/MyButton";
+import { stylesLogin } from "../views/style/StyleLogin";
 
 const uri =
   "https://images.unsplash.com/photo-1527517928481-bcf8d6534de0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fGF2aWFjaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
 
 export default function LoginScreen() {
-
   const [inputs, setInputs] = useState({
     email: "",
-    password: ""
+    password: "",
   });
-  
+
   const [errors, setErrors] = useState({});
   const [showPassword, setshowPassword] = useState(false);
-
 
   const validate = () => {
     Keyboard.dismiss();
@@ -42,8 +41,6 @@ export default function LoginScreen() {
       handleError("*Please input a valid email", "email");
       isValid = false;
     }
-
-    
 
     if (!inputs.password) {
       handleError("*Please input password", "password");
@@ -59,45 +56,34 @@ export default function LoginScreen() {
       handleError("*Please input a valid password", "password");
       isValid = false;
     }
-   
 
     if (isValid) {
       register();
     }
   };
 
-
-    //functions
-    const handleOnchange = (text, input) => {
-      setInputs((prevState) => ({ ...prevState, [input]: text }));
-    };
-    const handleError = (error, input) => {
-      setErrors((prevState) => ({ ...prevState, [input]: error }));
-    };
-
+  //functions
+  const handleOnchange = (text, input) => {
+    setInputs((prevState) => ({ ...prevState, [input]: text }));
+  };
+  const handleError = (error, input) => {
+    setErrors((prevState) => ({ ...prevState, [input]: error }));
+  };
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={stylesLogin.container}>
       <StatusBar style="light" />
-      <Image source={{ uri }} style={[styles.image, StyleSheet.absoluteFill]} />
-      <KeyboardAwareScrollView style={{ marginTop: 30, paddingTop: 30 }}>
-        <ScrollView
-          contentContainerStyle={{
-            flex: 1,
-            width: "100%",
-            height: "100%",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
+      <Image
+        source={{ uri }}
+        style={[stylesLogin.image, StyleSheet.absoluteFill]}
+      />
+      <KeyboardAwareScrollView style={stylesLogin.keyboard}>
+        <ScrollView contentContainerStyle={stylesLogin.contentContainer}>
           <BlurView intensity={70}>
-            <View style={styles.login}>
+            <View style={stylesLogin.login}>
               <LottieView
                 autoPlay
-                style={{
-                  width: 200,
-                  height: 170
-                }}
+                style={stylesLogin.lottieStyle}
                 source={require("../../assets/flights.json")}
               />
               <View>
@@ -124,23 +110,26 @@ export default function LoginScreen() {
                   onFocus={() => handleError(null, "password")}
                 />
 
-                <MyButton
-                  text={" Login "}
-                  alertText={"Press Btn Sing Up"}
-                  onPress={validate}
+                <View style={stylesLogin.viewStylesButtons}>
+                  <MyButton
+                    text={" Login "}
+                    alertText={"Press Btn Sing Up"}
+                    onPress={validate}
+                  />
 
-                />
+                  <MyButton
+                    text={"Sign Up with Google "}
+                    name={"google"}
+                    onPress={() => console.log("handleCreateAccount")}
+                  />
+                </View>
 
-                <MyButton
-                  text={"Sign Up with Google "}
-                  name={"google"}
-                  onPress={() => console.log("handleCreateAccount")}
-                />
-
-                <MyButton
-                  text={"Register "}
+                <Text
                   onPress={() => alert("Navigate Register")}
-                />
+                  style={stylesLogin.textStyles}
+                >
+                  Register
+                </Text>
               </View>
             </View>
           </BlurView>
@@ -150,56 +139,3 @@ export default function LoginScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#382E29",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  image: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "cover",
-    opacity: 0.45
-  },
-  login: {
-    width: "95%",
-    height: 650,
-    borderColor: "#fff",
-    borderWidth: 1,
-    borderRadius: 8,
-    padding: 10,
-    alignItems: "center",
-    opacity: 0.8
-  },
-  profilePicture: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    borderColor: "#382E29",
-    borderWidth: 1,
-    marginVertical: 30
-  },
-  input: {
-    width: 250,
-    height: 40,
-    borderColor: "#fff",
-    borderWidth: 2,
-    borderRadius: 10,
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: "#ffffff90",
-    marginBottom: 20
-  },
-  button: {
-    width: 250,
-    height: 40,
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    marginVertical: 10,
-    borderColor: "#fff",
-    borderWidth: 1
-  }
-});

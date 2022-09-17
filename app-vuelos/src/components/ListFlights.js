4c4202ef745c180d0dd94d3df57123b203a98d31
Jose Herrera <jherrera892016@gmail.com>
2022-09-16 19:00:28 -0500

    add (core): Add optimized ListFlights
    - Add Actions (Update and Delete) in component ListFlights
    
    co-authored-by: XimenaV26 <xvelasco1@ucol.mx>
    co-authored-by: Lucas-aquiles <lucasechegaray2011@gmail.com>

--- app-vuelos/src/components/ListFlights.js
+++ app-vuelos/src/components/ListFlights.js
@@ -1,5 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  TouchableOpacity
+} from "react-native";
 import React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 import { Colors } from "../theme/Colors";
 import { MaterialIcons } from "@expo/vector-icons";
 
@@ -7,45 +15,94 @@ const ListFlights = ({ data }) => {
   const { cityInitial, cityFinal, routeInitial, routeFinal, passengers, date } =
     data;
   return (
-    <View>
-      <View style={styles.citysCode}>
-        <Text style={{ fontWeight: "bold", fontSize: 25 }}>{routeInitial}</Text>
-        <MaterialIcons
-          name="flight"
-          size={24}
-          color={Colors.blue}
-          style={{
-            transform: [{ rotate: "90deg" }],
-            alignSelf: "center",
-            position: "absolute",
-            marginLeft: 160
-          }}
-        />
-        <Text style={{ fontWeight: "bold", fontSize: 25 }}>{routeFinal}</Text>
-      </View>
-      <View style={styles.citys}>
-        <Text style={{ fontSize: 12, color: Colors.grey }}>{cityInitial}</Text>
-        <Text style={{ fontSize: 12, color: Colors.grey, paddingBottom: 5 }}>
-          {cityFinal}
-        </Text>
-      </View>
-      <View style={styles.info}>
-        <Text style={{ fontSize: 12, color: Colors.black }}>{date}</Text>
-        <Text
-          style={{
-            fontSize: 12,
-            color: Colors.black,
-            paddingBottom: 18
-          }}
-        >
-          {`${passengers} Passengers`}
-        </Text>
-      </View>
-    </View>
+    <GestureHandlerRootView>
+      <Swipeable renderRightActions={swipeRightActions}>
+        <View style={styles.citysCode}>
+          <Text style={{ fontWeight: "bold", fontSize: 25 }}>
+            {routeInitial}
+          </Text>
+          <MaterialIcons
+            name="flight"
+            size={24}
+            color={Colors.blue}
+            style={{
+              transform: [{ rotate: "90deg" }],
+              alignSelf: "center",
+              position: "absolute",
+              marginLeft: 160
+            }}
+          />
+          <Text style={{ fontWeight: "bold", fontSize: 25 }}>{routeFinal}</Text>
+        </View>
+        <View style={styles.citys}>
+          <Text style={{ fontSize: 12, color: Colors.grey }}>
+            {cityInitial}
+          </Text>
+          <Text style={{ fontSize: 12, color: Colors.grey, paddingBottom: 5 }}>
+            {cityFinal}
+          </Text>
+        </View>
+        <View style={styles.info}>
+          <Text style={{ fontSize: 12, color: Colors.black }}>{date}</Text>
+          <Text
+            style={{
+              fontSize: 12,
+              color: Colors.black,
+              paddingBottom: 18
+            }}
+          >
+            {`${passengers} Passengers`}
+          </Text>
+        </View>
+      </Swipeable>
+    </GestureHandlerRootView>
   );
 };
 
 export default ListFlights;
+
+//function SwipeRight
+
+const swipeRightActions = (progress, dragX) => {
+  const scale = dragX.interpolate({
+    inputRange: [-100, 0],
+    outputRange: [0.7, 0]
+  });
+  return (
+    <>
+      <TouchableOpacity
+        onPress={() => alert("Delete Fligth")}
+        style={{ backgroundColor: "tomato", justifyContent: "center" }}
+      >
+        <Animated.Text
+          style={{
+            color: "white",
+            paddingHorizontal: 19,
+            fontWeight: "600",
+            transform: [{ scale }]
+          }}
+        >
+          <MaterialIcons name="delete-sweep" size={24} color={Colors.white} />
+        </Animated.Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => alert("Update Fligth")}
+        style={{ backgroundColor: "blue", justifyContent: "center" }}
+      >
+        <Animated.Text
+          style={{
+            color: "white",
+            paddingHorizontal: 19,
+            fontWeight: "600",
+            transform: [{ scale }]
+          }}
+        >
+          <MaterialIcons name="edit" size={24} color={Colors.white} />
+        </Animated.Text>
+      </TouchableOpacity>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   citysCode: {
